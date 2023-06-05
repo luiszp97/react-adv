c8e89e5e80db8833d5350755bef63f60dde9e1be
Luis <zambranoluis383@gmail.com>
2023-06-05 13:13:28 -0400

    control-props-alternativo

--- src/02-components-patterns/hooks/useProduct.ts
+++ src/02-components-patterns/hooks/useProduct.ts
@@ -13,14 +13,9 @@ export const useProduct = ( { onChange, product, value = 0 }: useProductArgs ) =
 
     const [counter, setCounter] = useState( value );
 
-    const isControlled = useRef( !!onChange )
+  
 
     const increaseBy = ( value: number ) => {
-
-      if( isControlled.current ){
-        
-        return onChange!( {count: value, product} )
-      }
 
       const newValue = Math.max( counter + value, 0 )
 
--- src/02-components-patterns/hooks/useShoppingCard.ts
+++ src/02-components-patterns/hooks/useShoppingCard.ts
@@ -9,30 +9,14 @@ export const useShoppingCart = ()=> {
     
     setShoppingCart( oldShoppingCard => {
 
-      const productInCart: ProductInCart = oldShoppingCard[product.id] || { ...product, count: 0 };
-
-      if( Math.max( productInCart.count + count, 0  ) > 0 ){
-        productInCart.count += count;
-        return {
-          ...oldShoppingCard,
-          [product.id]: productInCart
-        }
+      if( count === 0  ){
+        const { [product.id]: toDelete,  ...rest } = oldShoppingCard;
+        return rest;
       }
-
-      //! Borrar El producto
-
-      const { [product.id]: toDelete,  ...rest } = oldShoppingCard;
-      return rest;
-      
-    
-      // if( count === 0  ){
-      //   const { [product.id]: toDelete,  ...rest } = oldShoppingCard;
-      //   return rest;
-      // }
-      // return{ 
-      //   ...oldShoppingCard,
-      //   [product.id] : { ...product, count  }
-      // }
+      return{ 
+        ...oldShoppingCard,
+        [product.id] : { ...product, count  }
+      }
     } )
   }
 
